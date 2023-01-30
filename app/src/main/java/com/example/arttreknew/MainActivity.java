034a1e5f35ba4c80638b3341c72ff2385a7adeae
VinodMohan9 <117071032+VinodMohan9@users.noreply.github.com>
2023-01-30 17:01:47 +0800

    signup page to firebase
    firebase sign up page

--- app/src/main/java/com/example/arttreknew/MainActivity.java
+++ app/src/main/java/com/example/arttreknew/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.arttreknew;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
-    //create object of DatabaseReference class to access firebase's Realtime Database
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("");
 
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText password = findViewById(R.id.editTextTextPassword4);
         final EditText conPassword = findViewById(R.id.editTextTextPassword5);
         final ImageButton sign_up = findViewById(R.id.imageButton13);
-
+        mDatabase = FirebaseDatabase.getInstance("https://arttreknew-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        mRef = mDatabase.getReference("location");
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,22 +44,60 @@ public class MainActivity extends AppCompatActivity {
                 final String conPasswordTxt = conPassword.getText().toString();
 
                 // check if user fill all the fields before sending data to firebase
-                if(nameTxt.isEmpty() || emailTxt.isEmpty() || passwordTxt.isEmpty()){
+                if (nameTxt.isEmpty() || emailTxt.isEmpty() || passwordTxt.isEmpty()) {
                     Toast.makeText(MainActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                 }
 
                 // check if passwords are matching with each other
                 // if not matching with each other then show a toast message
-                else if(!passwordTxt.equals(conPasswordTxt)){
+                else if (!passwordTxt.equals(conPasswordTxt)) {
                     Toast.makeText(MainActivity.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
 
-            }
-                else{
+                } else {
 
-        });
+                    mRef.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
 
+                            // check if email is not registered before
+                            if(snapshot.hasChild(emailTxt)){
+                                Toast.makeText(MainActivity.this, "Email is already registered", Toast.LENGTH_SHORT).show())
+                        }
+                            else{
+
+                                //sending data to firebase Realtime Database.
+                                // we are using email as unique identity of every user.
+                                //so all the other details of user comes under email
+                                mRef.child("users").child(emailTxt).child("fullname").setValue(nameTxt);
+                                mRef.child("users").child(emailTxt).child("email").setValue(emailTxt);
+                                mRef.child("users").child(emailTxt).child("password").setValue(passwordTxt);
+
+                                // show a success message then finish the activity
+                                Toast.makeText(MainActivity.this, "User Registered Successfully.", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
+
+                }
             }
         });
+        return sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+
+        }
+    });
 
 
     }
