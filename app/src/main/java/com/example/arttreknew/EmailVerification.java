43333157db71adc5bd1f6041454716f324211709
Pavithransiva <pavithransiva@gmail.com>
2023-01-31 14:28:52 +0800

    change name

--- app/src/main/java/com/example/arttreknew/EmailVerification.java
+++ app/src/main/java/com/example/arttreknew/EmailVerification.java
@@ -9,6 +9,6 @@ public class EmailVerification extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.email_verificationpage);
+        setContentView(R.layout.email_verificationpagefromsignin);
     }
 }
