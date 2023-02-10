6878683652fb07fb9094bef8f4d2647a884e7e8e
Nicholas Choo Jia Jun <nicholaschoojiajun@gmail.com>
2023-02-11 00:49:25 +0800

    user page half done!

--- app/src/main/java/com/example/arttreknew/UserPage.java
+++ app/src/main/java/com/example/arttreknew/UserPage.java
@@ -9,12 +9,22 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
 import java.util.Objects;
 
 public class UserPage extends AppCompatActivity {
@@ -24,6 +34,8 @@ public class UserPage extends AppCompatActivity {
     private ImageView mBGImageView;
 
     private DatabaseReference mDatabaseRef;
+
+    private final String currUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +83,35 @@ public class UserPage extends AppCompatActivity {
             }
             return false;
 
+        });
+
+        ShapeableImageView userpage_bgimage = findViewById(R.id.userpage_bgimage);
+        TextView userpage_username = findViewById(R.id.userpage_username);
+        TextView userpage_jobtitle = findViewById(R.id.userpage_jobtitle);
+
+        DatabaseReference childRef = FirebaseDatabase.getInstance().getReference("location").child("users").child(currUserEmail.replace(".", "%"));
+        childRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String, Object> data = (HashMap<String, Object>) snapshot.getValue();
+                if (data.get("fullname") != null) {
+                    String dataFullname = (String) data.get("fullname");
+                    userpage_username.setText(dataFullname, TextView.BufferType.EDITABLE);
+                }
+                if (data.get("imageURL") != null) {
+                    String dataImageURL = (String) data.get("imageURL");
+                    Glide.with(getApplicationContext()).load(dataImageURL).into(userpage_bgimage);
+                }
+                if (data.get("title") != null) {
+                    String dataTitle = (String) data.get("title");
+                    userpage_jobtitle.setText(dataTitle, TextView.BufferType.EDITABLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 
