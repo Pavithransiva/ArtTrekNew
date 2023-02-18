d40049f45876944a3e6bb9e5658b24aaf55b7eb1
MarinCrazy <imrtyty@gmail.com>
2023-02-19 00:28:24 +0800

    Chat Pages Static FrontEnd

---
+++ app/src/main/java/com/example/arttreknew/ChatActionPage_D3.java
@@ -0,0 +1,34 @@
+package com.example.arttreknew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class ChatActionPage_D3 extends AppCompatActivity {
+
+    private Button bt_previous;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.chat_action_page_d3);
+
+        bt_previous = findViewById(R.id.chatactionp0_btn_previous);
+
+        bt_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatActionPage_D3.this, ChatLandingPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
